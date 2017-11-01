d3b9743e038411419d3dd47e8007df6dc55f6a97
jinke <1359518268@qq.com>
2017-11-01 17:16:09 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+/**
+ * @version 0.1.0
+ */
+
+import React, { PureComponent } from "react"
+import PropTypes from "prop-types"
+
+export default class ReactImageMagician extends PureComponent {
+  render(){
+    return [
+      <div>1</div>,
+      <div>2</div>
+    ]
+  }
+}
